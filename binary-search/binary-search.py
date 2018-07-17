82d8eaf81d6651428694fd92fb6f3f217d6c34b0
Sean Tan <shaqsnake@gmail.com>
2018-07-18 01:01:26 +0800

    update binary  search

--- binary-search/binary-search.py
+++ binary-search/binary-search.py
@@ -10,7 +10,22 @@ def binary_search(data, target, low, high):
     else:
         return binary_search(data, target, low, mid-1)
 
+def binary_search2(data, target, low, high):
+    while low < high:
+        mid = low + high // 2
+
+        if data[mid] == target:
+            return mid
+        if data[mid] < target:
+            low = mid + 1
+        else:
+            high = mid -1
+
+    return -1
+
+
 
 if __name__ == '__main__':
     l = [1, 2, 4, 6, 7, 8, 11, 15, 21, 27]
     print(binary_search(l, 2, 0, len(l)-1))
+    print(binary_search2(l, 2, 0, len(l)-1))
