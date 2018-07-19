51157dd1f8eb770fc844252de0f10045c0166352
t00361654 <tanzhuxiaqiu@huawei.com>
2018-07-19 14:49:05 +0800

    update 209

--- 209-Minimum-Size-Subarray-Sum/solution.py
+++ 209-Minimum-Size-Subarray-Sum/solution.py
@@ -5,22 +5,27 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        i = 0
-        j = -1
+        l = 0
+        r = -1
         sum = 0
         res = len(nums) + 1
         
-        while i < len(nums)-1:
-            if j < len(nums)-1 and sum < s:
-                j += 1
-                sum += nums[j]
-                continue
-                
+        while l < len(nums):
+            if r+1<len(nums) and sum < s:
+                r += 1
+                sum += nums[r]
+            else:
+                sum -= nums[l]
+                l += 1
+            
             if sum >= s:
-                res = min(res, j-i+1)
-                sum -= nums[i]
-            i += 1
+                res = min(res, r-l+1)
         
         if res == len(nums) + 1:
             return 0
         return res
+
+if __name__ == '__main__':
+    print(Solution().minSubArrayLen(11, [1, 2, 3, 4, 5]))
+    print(Solution().minSubArrayLen(7, [1, 2, 3, 4, 5]))
+    print(Solution().minSubArrayLen(100, []))
