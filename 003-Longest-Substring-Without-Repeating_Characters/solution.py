0eb27cfe50173eed24752363a512bff3b64e2045
t00361654 <tanzhuxiaqiu@huawei.com>
2018-07-19 15:20:34 +0800

    update 003

--- 003-Longest-Substring-Without-Repeating_Characters/solution.py
+++ 003-Longest-Substring-Without-Repeating_Characters/solution.py
@@ -29,6 +29,28 @@ class Solution(object):
             d[n] = i
         return res
 
+    def lengthOfLongestSubstring2(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        f = [0] * 256
+        l = 0
+        r = -1
+        res = 0
+
+        while l < len(s):
+            if r+1 < len(s) and f[ord(s[r+1])] == 0:
+                r += 1
+                f[ord(s[r])] += 1
+            else:
+                f[ord(s[l])] -= 1
+                l += 1
+            
+            res = max(res, r-l+1)
+
+        return res
 
 if __name__ == '__main__':
     print(Solution().lengthOfLongestSubstring("tmmzuxt"))
+    print(Solution().lengthOfLongestSubstring2("abcabcbb"))
