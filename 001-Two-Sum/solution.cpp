56c05801bd73f90dbd7a632f5928cebf0352860a
shaqsnake <shaqsnake@gmail.com>
2019-05-06 14:43:19 +0800

    Add cpp solution of #1.

---
+++ 001-Two-Sum/solution.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+
+using namespace std;
+
+class Solution
+{
+public:
+    vector<int> twoSum(vector<int> &nums, int target)
+    {
+        unordered_map<int, int> map;
+        for (decltype(nums.size()) i = 0; i < nums.size(); i++ ) {
+            if (map.find(target-nums[i]) != map.end()) {
+                return vector<int>{map[target-nums[i]], (int)i};
+            }
+            map[nums[i]] = i;
+        }
+
+        return vector<int>();
+    }
+};
+
+int main()
+{
+    vector<int> nums = {2, 7, 9, 11};
+    int target = 9;
+    vector<int> ret = Solution().twoSum(nums, target);
+
+    for (auto i : ret)
+    {
+        cout << i << " ";
+    }
+    cout << endl;
+    return 0;
+}
