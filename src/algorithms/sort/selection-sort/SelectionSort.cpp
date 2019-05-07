58ec51d320843f859c58897cc7c3645bd353338a
shaqsnake <shaqsnake@gmail.com>
2019-05-07 15:24:58 +0800

    Add SortUtils, and change other files' name.

--- src/algorithms/sort/selection-sort/SelectionSort.cpp
+++ src/algorithms/sort/selection-sort/SelectionSort.cpp
@@ -1,12 +1,13 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
-#include "student.h"
+#include "Student.h"
+#include "SortUtils.h"
 
 using namespace std;
 
 template<typename T>
-vector<T> selectionSort(vector<T> vec)
+vector<T>& selectionSort(vector<T> &vec)
 {
     for (decltype(vec.size()) i = 0; i < vec.size(); ++i)
     {
@@ -23,27 +24,23 @@ vector<T> selectionSort(vector<T> vec)
     return vec;
 }
 
-template<typename T>
-void print_vector(vector<T> vec) {
-    for (const auto &v : vec) {
-        cout << v << " ";
-    }
-    cout << endl;
-}
+
 
 int main()
 {
-    vector<int> vec(10);
-    generate(vec.begin(), vec.end(), []() { return rand() % 100; });
-    print_vector(selectionSort(vec));
+    int n = 10000;
+    vector<int> vec(n);
+    generate(vec.begin(), vec.end(), []() { return rand() % 10000; });
+    // SortUtils::printVector(selectionSort(vec));
+    SortUtils::testSort("Selection Sort", selectionSort, vec);
 
     vector<double> vec2 = {9.9, 8.8, 7.7, 1.1, 4.4, 5.5};
-    print_vector(selectionSort(vec2));
+    SortUtils::printVector(selectionSort(vec2));
 
     vector<string> vec3 = {"D", "C", "B", "A"};
-    print_vector(selectionSort(vec3));
+    SortUtils::printVector(selectionSort(vec3));
 
     vector<Student> vec4 = {{"A", 60}, {"B", 55}, {"C", 60}, {"D", 88}};
-    print_vector(selectionSort(vec4));
+    SortUtils::printVector(selectionSort(vec4));
     return 0;
 }
---
+++ src/algorithms/sort/selection-sort/SortUtils.h
@@ -0,0 +1,37 @@
+#ifndef SELECTION_SORT_SORT_UTILS_H
+#define SELECTION_SORT_SORT_UTILS_H
+
+#include <vector>
+#include <string>
+#include <ctime>
+
+namespace SortUtils
+{
+
+template <typename T>
+void printVector(std::vector<T> &vec)
+{
+    std::cout << "Sorted Vector: ";
+    for (const auto &v : vec)
+    {
+        std::cout << v << " ";
+    }
+    std::cout << std::endl;
+
+    return;
+}
+
+template <typename T>
+void testSort(const std::string &sortName, std::vector<T> &(*sort)(std::vector<T> &), std::vector<T> &vec)
+{
+    auto start = clock();
+    sort(vec);
+    auto end = clock();
+    std::cout << sortName << ": " << double(end - start) / CLOCKS_PER_SEC << "s" << std::endl;
+
+    return;
+}
+
+}; // namespace SortUtils
+
+#endif // SELECTION_SORT_SORT_UTILS_H
