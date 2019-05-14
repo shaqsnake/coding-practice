86ddbb43e0e1f43447bc73910172d7d74623ad7e
shaqsnake <shaqsnake@gmail.com>
2019-05-14 14:31:45 +0800

    Change dirname from bit to bitwise.

---
+++ src/algorithms/bitwise/a_mul_b.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+#define ll long long
+
+ll a_mul_b_mod_p(ll a , ll b, ll p) {
+    ll res = 0;
+    while(b) {
+        if (b&1)
+            res = (res + a) % p;
+        a = a * 2 % p;
+        b >>= 1;
+    }
+}
+
+int main() {
+    int a, b, p;
+    cin >> a >> b >> p;
+    cout << a_mul_b_mod_p(a, b, p) << endl;
+    return 0;
+}
