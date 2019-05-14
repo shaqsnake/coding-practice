e9a0ee719e5349fbd69469d74d7402aefb4a0606
shaqsnake <shaqsnake@gmail.com>
2019-05-14 10:50:08 +0800

    Add leetcode 111.

---
+++ src/leetcode/0111-Minimum-Depth-of-Binary-Tree/solution.py
@@ -0,0 +1,83 @@
+import math
+
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+
+class Solution:
+    def minDepth(self, root: TreeNode) -> int:
+        if not root:
+            return 0
+        res = math.inf
+        if root.left:
+            res = min(res, self.minDepth(root.left) + 1)
+        if root.right:
+            res = min(res, self.minDepth(root.right) + 1)
+        if res == math.inf:
+            res = 1
+        return res
+
+
+def stringToTreeNode(input):
+    input = input.strip()
+    input = input[1:-1]
+    if not input:
+        return None
+
+    inputValues = [s.strip() for s in input.split(',')]
+    root = TreeNode(int(inputValues[0]))
+    nodeQueue = [root]
+    front = 0
+    index = 1
+    while index < len(inputValues):
+        node = nodeQueue[front]
+        front = front + 1
+
+        item = inputValues[index]
+        index = index + 1
+        if item != "null":
+            leftNumber = int(item)
+            node.left = TreeNode(leftNumber)
+            nodeQueue.append(node.left)
+
+        if index >= len(inputValues):
+            break
+
+        item = inputValues[index]
+        index = index + 1
+        if item != "null":
+            rightNumber = int(item)
+            node.right = TreeNode(rightNumber)
+            nodeQueue.append(node.right)
+    return root
+
+
+def main():
+    import sys
+    import io
+
+    def readlines():
+        for line in io.TextIOWrapper(sys.stdin.buffer, encoding='utf-8'):
+            yield line.strip('\n')
+
+    lines = readlines()
+    while True:
+        try:
+            line = next(lines)
+            root = stringToTreeNode(line)
+
+            ret = Solution().minDepth(root)
+
+            out = str(ret)
+            print(out)
+        except StopIteration:
+            break
+
+
+if __name__ == '__main__':
+    main()
