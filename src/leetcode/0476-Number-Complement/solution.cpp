79b9b56523a9fe0561ddd4cdb1709f09015bfd43
shaqsnake <shaqsnake@gmail.com>
2019-05-16 10:41:16 +0800

    Add leetcode 476

---
+++ src/leetcode/0476-Number-Complement/solution.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Solution
+{
+public:
+    int findComplement(int num)
+    {
+
+        int res = 0, t = 0;
+        while (num)
+        {
+            res += !(num & 1) << t;
+            num >>= 1;
+            ++t;
+        }
+
+        return res;
+    }
+};
+
+int stringToInteger(string input)
+{
+    return stoi(input);
+}
+
+int main()
+{
+    string line;
+    while (getline(cin, line))
+    {
+        int num = stringToInteger(line);
+
+        int ret = Solution().findComplement(num);
+
+        string out = to_string(ret);
+        cout << out << endl;
+    }
+    return 0;
+}
