d3e7e8d8d8706d1e7a00188f1b5ab94cb4979345
shaqsnake <shaqsnake@gmail.com>
2019-05-16 13:49:10 +0800

    Add leetcode 371.

---
+++ src/leetcode/0371-Sum-of-Two-Integers/solution.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Solution
+{
+public:
+    int getSum(int a, int b)
+    {
+        if (b == 0)
+            return a;
+        return getSum(a ^ b, uint(a & b) << 1);
+    }
+};
+
+int stringToInteger(string input)
+{
+    return stoi(input);
+}
+
+int main()
+{
+    string line;
+    while (getline(cin, line))
+    {
+        int a = stringToInteger(line);
+        getline(cin, line);
+        int b = stringToInteger(line);
+
+        int ret = Solution().getSum(a, b);
+
+        string out = to_string(ret);
+        cout << out << endl;
+    }
+    return 0;
+}
