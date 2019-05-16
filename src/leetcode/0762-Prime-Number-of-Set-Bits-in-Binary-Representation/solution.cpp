5d8468d49b2bd6e930d390045122398c013b9669
shaqsnake <shaqsnake@gmail.com>
2019-05-16 10:03:39 +0800

    Add leetcode 762.

---
+++ src/leetcode/0762-Prime-Number-of-Set-Bits-in-Binary-Representation/solution.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Solution
+{
+public:
+    int countPrimeSetBits(int L, int R)
+    {
+        unordered_set<int> primes = {2, 3, 5, 7, 11, 13, 17, 19};
+
+        int res = 0;
+        for (int i = L; i <= R; ++i)
+        {
+            int bit_cnt = 0;
+            for (int j = i; j; j >>= 1)
+            {
+                bit_cnt += j & 1;
+            }
+            if (primes.count(bit_cnt))
+            {
+                ++res;
+            }
+        }
+
+        return res;
+    }
+};
+
+int stringToInteger(string input)
+{
+    return stoi(input);
+}
+
+int main()
+{
+    string line;
+    while (getline(cin, line))
+    {
+        int L = stringToInteger(line);
+        getline(cin, line);
+        int R = stringToInteger(line);
+
+        int ret = Solution().countPrimeSetBits(L, R);
+
+        string out = to_string(ret);
+        cout << out << endl;
+    }
+    return 0;
+}
