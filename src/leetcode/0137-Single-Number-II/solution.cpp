a869d5281a0aa324e2a0a164a3c757d55bbeec26
shaqsnake <shaqsnake@gmail.com>
2019-05-16 11:27:28 +0800

    Add leetcode 137.

---
+++ src/leetcode/0137-Single-Number-II/solution.cpp
@@ -0,0 +1,71 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Solution
+{
+public:
+    int singleNumber(vector<int> &nums)
+    {
+        int res = 0;
+        for (int i = 0; i < 32; i++)
+        {
+            int bit_cnt = 0;
+            for (auto &n : nums)
+            {
+                bit_cnt += (n >> i) & 1;
+            }
+            res += (bit_cnt % 3) << i;
+        }
+
+        return res;
+    }
+};
+
+void trimLeftTrailingSpaces(string &input)
+{
+    input.erase(input.begin(), find_if(input.begin(), input.end(), [](int ch) {
+                    return !isspace(ch);
+                }));
+}
+
+void trimRightTrailingSpaces(string &input)
+{
+    input.erase(find_if(input.rbegin(), input.rend(), [](int ch) {
+                    return !isspace(ch);
+                })
+                    .base(),
+                input.end());
+}
+
+vector<int> stringToIntegerVector(string input)
+{
+    vector<int> output;
+    trimLeftTrailingSpaces(input);
+    trimRightTrailingSpaces(input);
+    input = input.substr(1, input.length() - 2);
+    stringstream ss;
+    ss.str(input);
+    string item;
+    char delim = ',';
+    while (getline(ss, item, delim))
+    {
+        output.push_back(stoi(item));
+    }
+    return output;
+}
+
+int main()
+{
+    string line;
+    while (getline(cin, line))
+    {
+        vector<int> nums = stringToIntegerVector(line);
+
+        int ret = Solution().singleNumber(nums);
+
+        string out = to_string(ret);
+        cout << out << endl;
+    }
+    return 0;
+}
