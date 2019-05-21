d3cec1302f6e13c8e0a63839400f385b71f3334e
shaqsnake <shaqsnake@gmail.com>
2019-05-21 09:40:59 +0800

    Add 3-ways quick sort.

--- src/algorithms/sort/QuickSort.h
+++ src/algorithms/sort/QuickSort.h
@@ -13,7 +13,7 @@ void _quicksort(T a[], int l, int r)
     // int p = a[(l + r) >> 1]
     int p = a[rand() % (r - l + 1) + l]; // random pivot
     int i = l - 1, j = r + 1;
-    
+
     // two-ways quick sort
     while (i < j)
     {
@@ -41,6 +41,41 @@ void quickSort(T a[], int n)
     _quicksort(a, 0, n - 1);
 }
 
-}; // namespace MySort
+template <typename T>
+void _quicksort3w(T a[], int l, int r)
+{
+    if (l >= r)
+        return;
 
+    std::swap(a[l], a[rand() % (r - l + 1) + l]);
+    T p = a[l];
+
+    int lt = l;     // a[l+1, lt] < p
+    int gt = r + 1; // a[gt, r] > p
+    int i = l + 1;  // a[lt+1, i) == p
+    while (i < gt)
+    {
+        if (a[i] < p)
+            std::swap(a[++lt], a[i++]);
+        else if (a[i] > p)
+            std::swap(a[i], a[--gt]);
+        else
+            ++i;
+    }
+    std::swap(a[l], a[lt]);
+
+    _quicksort3w(a, l, lt - 1);
+    _quicksort3w(a, gt, r);
+
+    return;
+}
+
+template <typename T>
+void quickSort3Ways(T a[], int n)
+{
+    srand(time(NULL));
+    _quicksort3w(a, 0, n - 1);
+}
+
+};     // namespace MySort
 #endif // SORT_QUICKSORT_H
--- src/algorithms/sort/main.cpp
+++ src/algorithms/sort/main.cpp
@@ -22,6 +22,7 @@ int main()
     int *iarr5 = SortUtils::copyArray(iarr, n);
     int *iarr6 = SortUtils::copyArray(iarr, n);
     int *iarr7 = SortUtils::copyArray(iarr, n);
+    int *iarr8 = SortUtils::copyArray(iarr, n);
 
     // SortUtils::testSort("Selection Sort", MySort::selectionSort, iarr, n);
     // SortUtils::testSort("Insertion Sort", MySort::insertionSort, iarr1, n);
@@ -31,10 +32,12 @@ int main()
     SortUtils::testSort("Merge Sort", MySort::mergeSort, iarr5, n);
     SortUtils::testSort("Merge Sort BottomUp", MySort::mergeSort, iarr6, n);
     SortUtils::testSort("Quick Sort", MySort::quickSort, iarr7, n);
+    SortUtils::testSort("Quick Sort 3-Ways", MySort::quickSort3Ways, iarr8, n);
 
     delete[] iarr5;
     delete[] iarr6;
     delete[] iarr7;
+    delete[] iarr8;
     cout << endl;
 
     constexpr int swapTimes = 100;
@@ -42,34 +45,44 @@ int main()
     int *oarr = SortUtils::generateNearlyOrderedArray(n, swapTimes);
     int *oarr1 = SortUtils::copyArray(oarr, n);
     int *oarr2 = SortUtils::copyArray(oarr, n);
+    int *oarr3 = SortUtils::copyArray(oarr, n);
     SortUtils::testSort("Merge Sort", MySort::mergeSort, oarr, n);
     SortUtils::testSort("Merge Sort BottomUp", MySort::mergeSort, oarr1, n);
     SortUtils::testSort("Quick Sort", MySort::quickSort, oarr2, n);
+    SortUtils::testSort("Quick Sort 3-Ways", MySort::quickSort3Ways, oarr3, n);
 
     delete[] oarr;
     delete[] oarr1;
     delete[] oarr2;
+    delete[] oarr3;
     cout << endl;
 
     cout << "Test for random array with many duplication, size = " << n << ", random range [0, 9]" << endl;
     int *duarr = SortUtils::generateRandomArray(n, 0, 9);
     int *duarr1 = SortUtils::copyArray(duarr, n);
     int *duarr2 = SortUtils::copyArray(duarr, n);
+    int *duarr3 = SortUtils::copyArray(duarr, n);
     SortUtils::testSort("Merge Sort", MySort::mergeSort, duarr, n);
     SortUtils::testSort("Merge Sort BottomUp", MySort::mergeSort, duarr1, n);
     SortUtils::testSort("Quick Sort", MySort::quickSort, duarr2, n);
+    SortUtils::testSort("Quick Sort 3-Ways", MySort::quickSort3Ways, duarr3, n);
 
     delete[] duarr;
     delete[] duarr1;
     delete[] duarr2;
+    delete[] duarr3;
     cout << endl;
 
     double darr[10] = {9.9, 8.8, 7.7, 1.1, 4.4, 5.5, 2.2, 6.6, 0.0, 3.3};
     double *darr1 = SortUtils::copyArray(darr, 10);
+    double *darr2 = SortUtils::copyArray(darr, 10);
     MySort::bubbleSort(darr, 10);
     MySort::mergeSort(darr1, 10);
     SortUtils::printArray(darr, 10);
     SortUtils::printArray(darr1, 10);
+    MySort::quickSort3Ways(darr2, 10);
+    SortUtils::printArray(darr2, 10);
+    cout << endl;
 
     string sarr[4] = {"D", "C", "B", "A"};
     string *sarr1 = SortUtils::copyArray(sarr, 4);
@@ -77,10 +90,12 @@ int main()
     SortUtils::printArray(sarr, 4);
     MySort::mergeSort(sarr1, 4);
     SortUtils::printArray(sarr1, 4);
+    cout << endl;
 
     Student carr[4] = {{"A", 60}, {"B", 55}, {"C", 60}, {"D", 88}};
     MySort::mergeSort(carr, 4);
     SortUtils::printArray(carr, 4);
+    cout << endl;
 
     return 0;
 }
