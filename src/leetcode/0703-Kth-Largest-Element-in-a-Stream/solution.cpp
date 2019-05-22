a23632b73a82afb388bbd731b5d63e1f8f9ed7e6
shaqsnake <shaqsnake@gmail.com>
2019-05-22 16:35:19 +0800

    Add leetcode 703.

---
+++ src/leetcode/0703-Kth-Largest-Element-in-a-Stream/solution.cpp
@@ -0,0 +1,23 @@
+class KthLargest
+{
+public:
+    KthLargest(int k, vector<int> &nums) : kth(k)
+    {
+        for (const auto &n : nums)
+        {
+            add(n);
+        }
+    }
+
+    int add(int val)
+    {
+        pq.push(val);
+        while (pq.size() > kth)
+            pq.pop();
+        return pq.top();
+    }
+
+private:
+    priority_queue<int, vector<int>, greater<int>> pq;
+    int kth;
+};
