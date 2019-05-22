4d555499e2dcea0f6b26d21daad7db9b2f6d49e3
shaqsnake <shaqsnake@gmail.com>
2019-05-22 15:37:42 +0800

    Add state compression solution.

--- src/tedukuri/0x00/CH0303/solution.cpp
+++ src/tedukuri/0x00/CH0303/solution.cpp
@@ -29,9 +29,32 @@ void dfs(int x)
     }
 }
 
+void dfs2(int x, int state)
+{
+    if (x == n)
+    {
+        for (const auto &r : res)
+            cout << r << " ";
+        cout << endl;
+        return;
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        if (!(state >> i & 1))
+        {
+            res.push_back(i + 1);
+            dfs2(x + 1, state | (1 << i));
+            res.pop_back();
+        }
+    }
+}
+
 int main()
 {
     cin >> n;
     dfs(0);
+    cout << endl;
+    dfs2(0, 0);
     return 0;
 }
