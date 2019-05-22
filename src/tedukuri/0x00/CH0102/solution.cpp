4035b06c0398e1f444ac6f1e6a9f4946fe718a00
shaqsnake <shaqsnake@gmail.com>
2019-05-22 11:26:51 +0800

    Add ch0102.

---
+++ src/tedukuri/0x00/CH0102/solution.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+typedef long long ll;
+
+ll a_mul_b_mod_p(ll a, ll b, ll p)
+{
+    ll res = 0;
+    for (; b; b >>= 1)
+    {
+        if (b & 1)
+            res = (res + a) % p;
+        a = 2 * a % p;
+    }
+    return res;
+}
+
+int main()
+{
+    ll a, b, p;
+    cin >> a >> b >> p;
+
+    cout << a_mul_b_mod_p(a, b, p) << endl;
+}
