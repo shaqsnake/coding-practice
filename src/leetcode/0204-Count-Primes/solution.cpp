f36445fb96a6cba01f332096d80c74fc834c744e
shaqsnake <shaqsnake@gmail.com>
2019-07-19 17:40:58 +0800

    Update leetcode 204.

--- src/leetcode/0204-Count-Primes/solution.cpp
+++ src/leetcode/0204-Count-Primes/solution.cpp
@@ -1,3 +1,11 @@
+// Copyright (c) 2019 shaqsnake. All rights reserved.
+/**
+ * @Author: shaqsnake
+ * @Email: shaqsnake@gmail.com
+ * @Date: 2019-06-24 16:01:12
+ * @LastEditTime: 2019-07-19 17:40:43
+ * @Description:
+ */
 class Solution {
 public:
     int countPrimes(int n) {
@@ -14,5 +22,24 @@ public:
         }
 
         return res;
+    }
+
+    int countPrimes2(int n) {
+
+        bool st[n + 10] = {false, n + 10};
+        int primes[n + 10];
+        int idx = 0;
+
+        for (int i = 2; i < n; i++) {
+            if (!st[i])
+                primes[idx++] = i;
+            for (int j = 0; primes[j] <= n / i; j++) {
+                st[primes[j] * i] = true;
+                if (i % primes[j] == 0)
+                    break;
+            }
+        }
+
+        return idx;
     }
 };
