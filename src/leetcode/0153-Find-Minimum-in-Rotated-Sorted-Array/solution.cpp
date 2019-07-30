6cb96ddbf84ab015839a1db339a01fc8a4214cdd
shaqsnake <shaqsnake@gmail.com>
2019-07-30 16:23:12 +0800

    Update leetcode 153.

--- src/leetcode/0153-Find-Minimum-in-Rotated-Sorted-Array/solution.cpp
+++ src/leetcode/0153-Find-Minimum-in-Rotated-Sorted-Array/solution.cpp
@@ -3,7 +3,7 @@
  * @Author: shaqsnake
  * @Email: shaqsnake@gmail.com
  * @Date: 2019-07-27 17:51:05
- * @LastEditTime: 2019-07-27 17:53:06
+ * @LastEditTime: 2019-07-29 17:48:27
  * @Description: 153. Find Minimum in Rotated Sorted Array
  */
 class Solution {
@@ -21,5 +21,20 @@ public:
         }
 
         return nums[l];
+    }
+
+    int findMin2(vector<int> &nums) {
+        if (nums.back() > nums[0])
+            return nums[0];
+        int l = 0, r = nums.size() - 1;
+        while (l < r) {
+            int mid = l + r >> 1;
+            if (nums[mid] <= nums.back())
+                r = mid;
+            else
+                l = mid + 1;
+        }
+
+        return nums[r];
     }
 };
