7a5e05f3bb5618e63811add7a3422729ac28c4fd
shaqsnake <shaqsnake@gmail.com>
2019-08-05 17:23:07 +0800

    Update leetcode 162.

--- src/leetcode/0162-Find-Peak-Element/solution.cpp
+++ src/leetcode/0162-Find-Peak-Element/solution.cpp
@@ -3,19 +3,34 @@
  * @Author: shaqsnake
  * @Email: shaqsnake@gmail.com
  * @Date: 2019-08-01 17:38:47
- * @LastEditTime: 2019-08-01 17:40:40
+ * @LastEditTime: 2019-08-05 17:22:47
  * @Description: 162. Find Peak Element
  */
 class Solution {
 public:
-    int findPeakElement(vector<int>& nums) {
+    int findPeakElement(vector<int> &nums) {
         int l = 0, r = nums.size() - 1;
         while (l < r) {
             int mid = l + r + 1 >> 1;
-            if (nums[mid-1] <= nums[mid]) l = mid;
-            else r = mid - 1;
+            if (nums[mid - 1] <= nums[mid])
+                l = mid;
+            else
+                r = mid - 1;
         }
 
         return r;
+    }
+
+    int findPeakElement2(vector<int> &nums) {
+        int l = 0, r = nums.size() - 1;
+        while (l < r) {
+            int mid = l + r >> 1;
+            if (nums[mid] > nums[mid + 1])
+                r = mid;
+            else
+                l = mid + 1;
+        }
+
+        return l;
     }
 };
