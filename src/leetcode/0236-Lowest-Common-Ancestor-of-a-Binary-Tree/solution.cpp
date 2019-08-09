5ab40ac05e5da124bf5aac6a503735b8564a9ef0
shaqsnake <shaqsnake@gmail.com>
2019-08-09 11:21:45 +0800

    Add leetcode 236.

---
+++ src/leetcode/0236-Lowest-Common-Ancestor-of-a-Binary-Tree/solution.cpp
@@ -0,0 +1,31 @@
+// Copyright (c) 2019 shaqsnake. All rights reserved.
+/**
+ * @Author: shaqsnake
+ * @Email: shaqsnake@gmail.com
+ * @Date: 2019-08-09 11:19:01
+ * @LastEditTime: 2019-08-09 11:21:22
+ * @Description: 236. Lowest Common Ancestor of a Binary Tree
+ */
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        if (!root || root == p || root == q) return root;
+
+        auto left = lowestCommonAncestor(root->left, p, q);
+        auto right = lowestCommonAncestor(root->right, p, q);
+
+        if (!left) return right;
+        if (!right) return left;
+        return root;
+    }
+};
