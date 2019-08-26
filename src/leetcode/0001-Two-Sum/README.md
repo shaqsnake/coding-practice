2dbecf661e41ae81fbdc1fe2f36c386d116c3c05
shaqsnake <shaqsnake@gmail.com>
2019-08-26 10:31:05 +0800

    Update leetcode 1.

--- src/leetcode/0001-Two-Sum/README.md
+++ src/leetcode/0001-Two-Sum/README.md
@@ -1,3 +1,11 @@
+<!--
+ * @Author: shaqsnake
+ * @Email: shaqsnake@gmail.com
+ * @Date: 2019-05-09 13:47:55
+ * @LastEditTime: 2019-08-26 10:27:46
+ * @Description: 1. Two Sum
+ -->
+
 Given an array of integers, return indices of the two numbers such that they add up to a specific target.
 
 You may assume that each input would have exactly one solution, and you may not use the same element twice.
--- src/leetcode/0001-Two-Sum/solution.cpp
+++ src/leetcode/0001-Two-Sum/solution.cpp
@@ -1,8 +1,11 @@
-#include <iostream>
-#include <unordered_map>
-#include <vector>
-
-using namespace std;
+// Copyright (c) 2019 shaqsnake. All rights reserved.
+/**
+ * @Author: shaqsnake
+ * @Email: shaqsnake@gmail.com
+ * @Date: 2019-06-10 17:41:50
+ * @LastEditTime: 2019-08-26 10:30:11
+ * @Description: 1. Two Sum
+ */
 
 class Solution {
 public:
@@ -17,16 +20,14 @@ public:
 
         return vector<int>();
     }
-};
 
-int main() {
-    vector<int> nums = {2, 7, 9, 11};
-    int target = 9;
-    vector<int> ret = Solution().twoSum(nums, target);
+    vector<int> twoSum2(vector<int>& nums, int target) {
+        unordered_map<int, int> m;
+        for (int i = 0; i < nums.size(); i++) {
+            if (m.count(target - nums[i])) return {m[target - nums[i]], i};
+            m[nums[i]] = i;
+        }
 
-    for (auto i : ret) {
-        cout << i << " ";
+        return {-1, -1};
     }
-    cout << endl;
-    return 0;
-}
+};
