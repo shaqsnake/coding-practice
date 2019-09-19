d41ae707eee2c61d92096aef6b4bc9815f5ec4f7
shaqsnake <shaqsnake@gmail.com>
2019-09-19 14:46:29 +0800

    Update acwing 1015.

--- src/acwing/1015/solution.cpp
+++ src/acwing/1015/solution.cpp
@@ -3,7 +3,7 @@
  * @Author: shaqsnake
  * @Email: shaqsnake@gmail.com
  * @Date: 2019-09-18 19:04:24
- * @LastEditTime: 2019-09-18 19:10:54
+ * @LastEditTime: 2019-09-19 14:46:11
  * @Description: Acwing 1015
  */
 #include <cstring>
@@ -18,8 +18,6 @@ int main() {
     int t;
     cin >> t;
     while (t--) {
-        memset(f, 0, sizeof f);
-
         int n, m;
         cin >> n >> m;
         for (int i = 1; i <= n; i++)
@@ -27,7 +25,6 @@ int main() {
                 cin >> a[i][j];
 
         int res = 0;
-        f[1][1] = a[1][1];
         for (int i = 1; i <= n; i++)
             for (int j = 1; j <= m; j++)
                 f[i][j] = max(f[i - 1][j], f[i][j - 1]) + a[i][j];
