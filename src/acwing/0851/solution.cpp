3a7a3dc6330577d9d48d301ffc43d77e28c46418
Sean Tan <shaqsnake@gmail.com>
2019-10-18 21:52:40 +0800

    Add acwing 851.

---
+++ src/acwing/0851/solution.cpp
@@ -0,0 +1,8 @@
+// Copyright (c) 2019 shaqsnake. All rights reserved.
+/**
+ * @Author: shaqsnake
+ * @Email: shaqsnake@gmail.com
+ * @Date: 2019-10-17 09:55:08
+ * @LastEditTime: 2019-10-17 10:41:59
+ * @Description: Acwing 851
+ */
