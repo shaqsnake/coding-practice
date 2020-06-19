bd52ca508907f3a91510e7fa43910e7c984d77d5
shaqsnake <shaqsnake@gmail.com>
2020-06-19 10:59:51 +0800

    Add facade.

---
+++ src/design-patterns/facade/main.cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+
+class Subsystem1 {
+public:
+    std::string Operation1() const {
+        return "Subsystem1: Ready!\n";
+    }
+    // ...
+    std::string OperationN() const {
+        return "Subsystem1: Go!\n";
+    }
+};
+
+class Subsystem2 {
+public:
+    std::string Operation1() const {
+        return "Subsystem2: Get ready!\n";
+    }
+    // ...
+    std::string OperationZ() const {
+        return "Subsystem2: Fire!\n";
+    }
+};
+
+class Facade {
+protected:
+    Subsystem1 *subsystem1_;
+    Subsystem2 *subsystem2_;
+
+public:
+    Facade(Subsystem1 *subsystem1 = nullptr, Subsystem2 *subsystem2 = nullptr) {
+        subsystem1_ = subsystem1 ? : new Subsystem1;
+        subsystem2_ = subsystem2 ? : new Subsystem2;
+    }
+    ~Facade() {
+        delete subsystem1_;
+        delete subsystem2_;
+    }
+
+    std::string Operation() {
+        std::string result = "Facade initializes subsystems:\n";
+        result += subsystem1_->Operation1();
+        result += subsystem2_->Operation1();
+        result += "Facade orders subsystems to perform the action:\n";
+        result += subsystem1_->OperationN();
+        result += subsystem2_->OperationZ();
+        return result;
+    }
+};
+
+void ClientCode(Facade *facade) {
+    std::cout << facade->Operation();
+}
+
+int main() {
+    Subsystem1 *subsystem1 = new Subsystem1;
+    Subsystem2 *subsystem2 = new Subsystem2;
+    Facade *facade = new Facade(subsystem1, subsystem2);
+    ClientCode(facade);
+
+    delete facade;
+
+    return 0;
+}
